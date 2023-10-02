f2638699ee92c41f3d029daa92d88a283a43bda8
Zzang-yeah <71954155+Zzang-yeah@users.noreply.github.com>
2023-10-02 17:26:00 +0900

    .
    .

--- lib/startPage/startPage.dart
+++ lib/startPage/startPage.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:mbti_test/startPage/startPage.dart';
 
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class startPage extends StatelessWidget {
+  const startPage({super.key, required String title});
 
   // This widget is the root of your application.
   @override
@@ -15,7 +9,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const startPage(title: 'Flutter Demo Home Page'),
